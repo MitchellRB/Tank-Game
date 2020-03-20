f888b9df64e23b01a5ec88762db8d1cc7f6cf742
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-03-20 14:37:28 +1100

    Vector2 operators

--- MathClasses/Vector2.cs
+++ MathClasses/Vector2.cs
@@ -21,5 +21,25 @@ namespace MathClasses
             x = _x;
             y = _y;
         }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x + v2.x, v1.y + v2.y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public static Vector2 operator *(Vector2 v, float f)
+        {
+            return new Vector2(v.x * f, v.y * f);
+        }
+
+        public static Vector2 operator *(float f, Vector2 v)
+        {
+            return new Vector2(v.x * f, v.y * f);
+        }
     }
 }
