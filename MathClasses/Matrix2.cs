543e955fe34f476f131ed4f93f180b0e1f1991ae
MitchellRB <mitchell.r.bonham@gmail.com>
2020-03-25 11:20:36 +1100

    Matrix2 class

---
+++ MathClasses/Matrix2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    class Matrix2
+    {
+        public float m1, m2, m3, m4;
+
+        public Matrix2()
+        {
+            m1 = 0; m2 = 0;
+            m3 = 0; m4 = 0;
+        }
+    }
+}
